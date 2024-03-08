c118186e89afe434bb7de0eb5d678d1e4e6e57e1
Juanique Voogt <juanique@kartoza.com>
2024-03-08 10:36:26 +0200

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Kartoza Shop
+
+Enchance current e commerce sttings
+
+#### License
+
+MIT
---
+++ kartoza_shop/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Kartoza Shop",
+			"type": "module",
+			"label": _("Kartoza Shop")
+		}
+	]
